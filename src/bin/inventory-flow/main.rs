e05786d080ee3771a5851dbbea19de453c165804
Thomas Gehrmann <thgehr@gmail.com>
2026-01-12 22:15:32 +0100

    Add instruction to update flows

--- src/bin/inventory-flow/main.rs
+++ src/bin/inventory-flow/main.rs
@@ -4,6 +4,8 @@
 // Listen to twob market price updates
 // - recalculate at which slot flows should be updated
 
+use std::time::Duration;
+
 // When update flow timer is triggered, update flows
 use anchor_client::{
     Client, Cluster,
@@ -12,11 +14,13 @@ use anchor_client::{
     },
 };
 use anchor_lang::prelude::*;
+use tokio::time::sleep;
 use twob_market_making::AccountResolver;
 
 use crate::twob_anchor::accounts::{Bookkeeping, LiquidityPosition, Market};
 
 declare_program!(twob_anchor);
+use twob_anchor::{client::accounts, client::args};
 
 const BOOKKEEPING_PRECISION_FACTOR: u128 = 1_000_000_000_000_000;
 const FLOW_PRECISION: u128 = 1_000_000_000;
@@ -55,6 +59,61 @@ async fn main() -> anyhow::Result<()> {
         .account::<Bookkeeping>(bookkeeping_pda.address())
         .await?;
 
+    // Update flows every x minutes
+    let update_flows_task = tokio::spawn(async move {
+        loop {
+            sleep(Duration::from_mins(1)).await;
+
+            let liquidity_provider = liquidity_provider.clone();
+            let program = client.program(twob_anchor::ID).unwrap();
+
+            let current_slot = program.rpc().get_slot().await.unwrap();
+            let reference_index = current_slot / 1000;
+
+            let current_exits_pda = resolver.exits_pda(&market_pda.address(), reference_index);
+            let previous_exits_pda = resolver.exits_pda(&market_pda.address(), reference_index - 1);
+
+            let current_prices_pda = resolver.prices_pda(&market_pda.address(), reference_index);
+            let previous_prices_pda =
+                resolver.prices_pda(&market_pda.address(), reference_index - 1);
+
+            let update_flows_ix = program
+                .request()
+                .accounts(accounts::UpdateLiquidityFlows {
+                    authority: liquidity_provider.pubkey(),
+                    market: market_pda.address(),
+                    liquidity_position: liquidity_position_pda.address(),
+                    bookkeeping: bookkeeping_pda.address(),
+                    current_exits: current_exits_pda.address(),
+                    previous_exits: previous_exits_pda.address(),
+                    current_prices: current_prices_pda.address(),
+                    previous_prices: previous_prices_pda.address(),
+                    system_program: system_program::ID,
+                })
+                .args(args::UpdateLiquidityFlows {
+                    reference_index: reference_index,
+                    base_flow_u64: 1,
+                    quote_flow_u64: 1,
+                })
+                .instructions()
+                .unwrap()
+                .remove(0);
+
+            if let Err(e) = program
+                .request()
+                .instruction(update_flows_ix)
+                .signer(liquidity_provider)
+                .send()
+                .await
+            {
+                eprintln!("Failed to update flows: {}", e);
+            };
+        }
+    });
+
+    ////////
+    // TODO: Update flows if slots until debt is smaller than y (maybe around 1000)
+    ////////
     // Calculate balances
     let current_slot = program.rpc().get_slot().await?;
 
@@ -136,5 +195,7 @@ async fn main() -> anyhow::Result<()> {
     println!("Current slot {}", current_slot);
     println!("Liquidity position {:?}", liquidity_position);
     println!("Bookkeeping {:?}", bookkeeping);
+
+    tokio::try_join!(update_flows_task)?;
     Ok(())
 }
